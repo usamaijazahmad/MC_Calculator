f0dad7f192810f8795576e7d23fc302c6105dc7e
Usama Ijaz <usamaijaz2001@gmail.com>
2022-04-21 20:25:06 +0500

    Did some work on java code.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,127 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btn7;
+    Button btn8;
+    Button btn9;
+    Button divbtn;
+    Button mulbtn;
+    Button plusbtn;
+    Button minbtn;
+    Button perc;
+    Button acbtn;
+    Button eqbtn;
+    Button delbtn;
+    Button zerobtn;
+    Button point;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn7=findViewById(R.id.btn7);
+        btn8=findViewById(R.id.btn8);
+        btn9=findViewById(R.id.btn9);
+        zerobtn=findViewById(R.id.zerobtn);
+        minbtn=findViewById(R.id.minbtn);
+        plusbtn=findViewById(R.id.plusbtn);
+        divbtn=findViewById(R.id.divbtn);
+        mulbtn=findViewById(R.id.mulbtn);
+        eqbtn=findViewById(R.id.eqbtn);
+        point=findViewById(R.id.point);
+        perc=findViewById(R.id.perc);
+        delbtn=findViewById(R.id.delbtn);
+        acbtn=findViewById(R.id.acbtn);
+        tv=findViewById(R.id.textView);
+
+        zerobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '0');
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '1');
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '2');
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '3');
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '4');
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '5');
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '6');
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '7');
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '8');
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(tv.getText().toString() + '9');
+            }
+        });
+
     }
 }
